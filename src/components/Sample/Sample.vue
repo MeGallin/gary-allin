21021cd5565354921686904033108922aba41576
Guy <me@garyallin.uk>
2020-12-28 10:57:50 +0000

    ga/sample: Sample Component

---
+++ src/components/Sample/Sample.vue
@@ -0,0 +1,33 @@
+<template>
+  <section id="sample">
+
+<div class="wrapper">
+  <div class="colour-one border-temp"><h1>{{msg}}</h1>
+<p>{{text}}</p></div>
+  <div class="colour-two"><h1>{{msg}}</h1>
+<p>{{text}}</p></div>
+  <div class="colour-three"><h1>{{msg}}</h1>
+<p>{{text}}</p></div>
+  <div class="colour-four"><h1>{{msg}}</h1>
+<p>{{text}}</p></div>
+</div>
+
+  </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: 'Sample',
+            text: 'This is my sample text'
+        }
+    },    
+    props: {
+            msg: String
+        }
+
+}
+</script>
+
+<style scoped src='./Sample.css'></style>
