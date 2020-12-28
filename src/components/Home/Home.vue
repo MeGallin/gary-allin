2de936013f64c88a0f4ab690b5d32f73deb4ff36
Guy <me@garyallin.uk>
2020-12-28 17:57:55 +0000

    ga/axios-get: get working

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,31 +1,57 @@
 <template>
-  <section id="home">
+<section id="home">
+  
+  <h1>{{name}}</h1>
+  
+  <div class="wrapper" > 
+     
+      <div class="colour-one border-temp" v-for="(des) in descriptions" :key="des.id">
+        <h3>{{des.title}}</h3>
+        <p>{{des.description}}</p>
+        {{loading}}{{loaded}}
+      </div>    
+  </div>
 
-<div class="wrapper">
-  <div class="colour-one border-temp"><h1>{{msg}}</h1>
-<p>{{text}}</p></div>
-  <div class="colour-two"><h1>{{msg}}</h1>
-<p>{{text}}</p></div>
-  <div class="colour-three"><h1>{{msg}}</h1>
-<p>{{text}}</p></div>
-  <div class="colour-four"><h1>{{msg}}</h1>
-<p>{{text}}</p></div>
-</div>
+  <div class="colour-two">
+    <p>{{text}}</p></div>
+  <div class="colour-three">
+    <p>{{text}}</p></div>
+  <div class="colour-four">
+    <p>{{text}}</p>
+  </div>
 
-  </section>
+</section>
 </template>
 
 <script>
+
 export default {
+ async created() {
+   const url = '../assets/Home/home.json';
+    try {
+          const headers = { "Content-Type": "application/json" };   
+          const data = await this.$http.get( url, { headers });
+          this.descriptions = data.data;
+          const status = data.status;
+          const statusText = data.statusText;
+          if(status !== 200) {
+            this.loading = 'loading...';
+          } else {
+            this.loaded = statusText;
+          }
+    } catch (error) {
+      console.error(error);
+    }
+  },
     data() {
         return {
             name: 'Home',
-            text: 'This is my HOME text'
+            text: 'This is my HOME text',
+            descriptions: [],
+            loading: '',
+            loaded: ''
         }
-    },    
-    props: {
-            msg: String
-        }
+    }
 
 }
 </script>
