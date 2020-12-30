26df85cab50590ca512bea78c2ffaa7e8171524b
Guy <me@garyallin.uk>
2020-12-30 16:26:11 +0000

    ga/logo: Working

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <section id="header">
     <h1>{{name}}</h1>
+    <Logo />
     <router-link to='/home'>Home</router-link>
     <router-link to='/about'>About</router-link>  
     <router-link to='/contact'>Contact Me</router-link>   
@@ -8,12 +9,16 @@
   </section>
 </template>
 <script>
+import Logo from '../common/Logo/Logo'
 export default {
     data() {
         return {
             name: 'Header',
             
         }
+    },
+    components: {
+      Logo
     }
 }
 </script>
---
+++ src/components/common/Logo/Logo.vue
@@ -0,0 +1,20 @@
+<template>
+<div id="logo">
+    <div class="navbar-brand link link--ilin">
+        <a class=" " href="#/"><span>{{name}}</span><span>{{surname}}</span></a> 
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: 'GARY',
+            surname: 'ALLIN'
+        }
+    }
+}
+</script>
+
+<style scoped src='./Logo.css'></style>
