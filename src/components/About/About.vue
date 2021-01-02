f86726d96f5b7c68e2b0cd350010c03e638be43c
Guy <me@garyallin.uk>
2021-01-02 12:52:45 +0000

    ga/tell-me-tweaks: updated

--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -1,23 +1,6 @@
 <template>
-  <section id="about">
-
-<div class="wrapper">
-  <div class="colour-one border-temp">
-         <div class="call_me">
-        <h1>
-          <span class="char1">T</span>
-          <span class="char2 tint2">h</span>
-          <span class="char3">i</span>
-          <span class="char4 tint4">s</span>
-          <span class="char5"> </span>
-          <span class="char5 tint1">i</span>
-          <span class="char7">s</span>
-          <span class="char8 tint3"></span>
-          <span class="char9">m</span>
-          <span class="char10 tint2">e</span>
-        </h1>      
-      </div>
-
+<div>  
+  <TellMe char1='T' char2='h' char3='i' char4='s' char5='is' char6='M' char7='e'></TellMe>
     <div class="wrapper" > 
       <div v-if="isLoading">
          <Spinner/>
@@ -25,24 +8,20 @@
         <!-- // Use this to get data with getters -->
         <!-- // {{getterDescriptions}} -->
     <div v-else class="colour-one border-temp" v-for="(des) in aboutDescriptions" :key="des.id">
- 
-
-            <h3>{{des.title}}</h3>
-            <Age/>
-            <p>{{des.description}}</p>
-            
-          </div>    
-      </div>
-  </div>    
+        <h3>{{des.title}}</h3>
+          <Age/>
+          <p>{{des.description}}</p>
+              
+    </div>    
+  </div>
 </div>
-
-  </section>
 </template>
 
 <script>
 import $Store from '../../store/index';
 import Spinner from '../common/LoadingSpinner/LoadingSpinner';
 import Age from '../common/Age/Age';
+import TellMe from '../common/TellMe/TellMe';
 export default {
     data() {
         return {
@@ -52,7 +31,8 @@ export default {
     },
     components: {
       Spinner,
-      Age
+      Age,
+      TellMe
     },
     computed: {
       getterAboutDescriptions() {
