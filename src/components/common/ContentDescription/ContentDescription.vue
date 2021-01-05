aa46496618bc83956a150be3609d1583ac0dfca2
Guy <me@garyallin.uk>
2021-01-05 12:41:41 +0000

    ga/content-wrapper: Slots

--- src/components/common/ContentDescription/ContentDescription.vue
+++ src/components/common/ContentDescription/ContentDescription.vue
@@ -1,12 +1,14 @@
 <template>
-  <div>
+  <content-wrapper>
     <h3>{{ title }}</h3>
     <p>{{ description }}</p>
-  </div>
+  </content-wrapper>
 </template>
 
 <script>
+import ContentWrapper from '../ContentWrapper/ContentWrapper.vue';
 export default {
+  components: { ContentWrapper },
   data() {
     return {
       name: 'ContentDescription',
